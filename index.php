b0a6fb8e900ac3d5923e828dd13ffe8329088340
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-09 23:59:53 +0300

    added login and signin php files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>The Book Testing</h1>
+    <button><a href="./login.php">Login</a></button>
+    <button><a href="./signin.php">Signin</a></button>
+</body>
+</html>

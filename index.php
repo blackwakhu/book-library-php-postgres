581a9129de7dcd4deb9a670eb97d8d9973c8660a
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-10 00:02:45 +0300

    rename some files

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
 <body>
     <h1>The Book Testing</h1>
     <button><a href="./login.php">Login</a></button>
-    <button><a href="./signin.php">Signin</a></button>
+    <button><a href="./signup.php">Sign up</a></button>
 </body>
 </html>

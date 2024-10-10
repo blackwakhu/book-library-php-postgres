4bb57b7c1b06b66986033d5404d0071070c85839
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-10 09:17:40 +0300

    added person details and form to signup.php

--- person/person.php
+++ person/person.php
@@ -7,13 +7,17 @@ class Person {
     private string $u_name;
     private string $passwrd;
     private string $dob;
+    private string $email;
+    private string $contact;
 
-    public function __construct ($f_name, $l_name, $u_name, $passwrd, $dob)  {
+    public function __construct ($f_name, $l_name, $u_name, $passwrd, $dob, $email, $contact)  {
         $this->f_name = $f_name;
         $this->l_name = $l_name;
         $this->u_name = $u_name;
         $this->passwrd = $passwrd;
         $this->dob = $dob;
+        $this->email = $email;
+        $this->contact = $contact;
     }
 }
 
---
+++ signup.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up</title>
+</head>
+<body>
+    <h1>Sign Up</h1>
+    <div>
+        <form action="" method="post">
+            <label for="fname">First Name</label><br>
+            <input type="text" name="fname" id=""><br>
+
+            <label for="lname">Last Name</label><br>
+            <input type="text" name="" id=""><br>
+
+            <label for="uname">User Name</label><br>
+            <input type="text" name="uname" id=""><br>
+
+            <label for="passwrd">Password</label><br>
+            <input type="password" name="passwrd" id=""><br>
+
+            <label for="dob">Date of Birth</label><br>
+            <input type="date" name="dob" id=""><br>
+
+            <label for="email">Email</label><br>
+            <input type="email" name="email" id=""><br>
+
+            <label for="contact">Contact</label><br>
+            <input type="tel" name="contact" id=""><br>
+
+            <input type="submit" value="submit">
+            <button><a href="./login">Login</a></button>
+
+        </form>
+    </div>
+</body>
+</html>

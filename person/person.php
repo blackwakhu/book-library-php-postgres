3cf5f27730200b3e8900de37fe56b17a3a17f492
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-10 09:06:47 +0300

    added database.php

--- person/person.php
+++ person/person.php
@@ -1,7 +1,22 @@
 <?php 
 
-class Person{
-    public function __construct(){}
+class Person {
+
+    private string $f_name;
+    private string $l_name;
+    private string $u_name;
+    private string $passwrd;
+    private string $dob;
+
+    public function __construct ($f_name, $l_name, $u_name, $passwrd, $dob)  {
+        $this->f_name = $f_name;
+        $this->l_name = $l_name;
+        $this->u_name = $u_name;
+        $this->passwrd = $passwrd;
+        $this->dob = $dob;
+    }
 }
+
+
 
 ?>

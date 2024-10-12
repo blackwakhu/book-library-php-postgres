0fde1adc65f995eda37d94877b5f3c0c757ce14e
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-12 16:01:44 +0300

    alter $getenv to getenv in database.php file

--- database/database.php
+++ database/database.php
@@ -1,12 +1,12 @@
 <?php 
 
-require_once "dbconfig.php";
+require "dbconfig.php";
 
 function getConnection ()  {
-    $username = $getenv('DB_USERNAME');
-    $password = $getenv('DB_PASSWORD');
-    $hostname = $getenv('DB_HOST');
-    $database = $getenv('DB_NAME');
+    $username = getenv('DB_USERNAME');
+    $password = getenv('DB_PASSWORD');
+    $hostname = getenv('DB_HOST');
+    $database = getenv('DB_NAME');
 
     $db = new PDO("pgsql:host=$hostname;dbname=$database", $username, $password);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
--- login.php
+++ login.php
@@ -4,14 +4,17 @@ require "./person/person.php";
 
 session_start();
 
+$us = "nothing set";
+
 
 if (isset($_POST["submit"]))  {
+    $us = "submit post button pressed";
     $person = Person::get_Person($_POST["uname"], $_POST["passwrd"]);
-    if (!empty($person)) {
-        $_SESSION['person'] = serialize($person);
-        header("Location: dashboard.php");
-        exit();
-    }
+    // if (!empty($person)) {
+    //     $_SESSION['person'] = serialize($person);
+    //     header("Location: dashboard.php");
+    //     exit();
+    // }
 }
 
 
@@ -28,15 +31,15 @@ if (isset($_POST["submit"]))  {
     <h1>Login</h1>
     
     <div>
-        <form action="login.php" method="post">
+        <form action="./login.php" method="post">
             <label for="uname">Username</label><br>
             <input type="text" name="uname" id=""><br>
             <label for="passwrd">Password</label><br>
             <input type="password" name="passwrd" id=""><br>
-            <input type="submit" value="login">
+            <input type="submit" name="submit" value="login">
             <button><a href="./signup.php">sign up</a></button>
         </form>
     </div>
-    
+    <p><?= $us ?></p>
 </body>
 </html>

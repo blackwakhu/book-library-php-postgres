295ad7de8510ae6c645cd64f5f0ccd75a50327a7
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-12 12:47:15 +0300

    added the env file

--- database/database.php
+++ database/database.php
@@ -1,10 +1,14 @@
 <?php 
-function getConnection ()  {
-    $username = "shibero";
-    $password = "wakhu";
-    $hostname = "localhost";
 
-    $db = new PDO("pgsql:host=$hostname;dbname=phptest", $username, $password);
+require_once "dbconfig.php";
+
+function getConnection ()  {
+    $username = $getenv('DB_USERNAME');
+    $password = $getenv('DB_PASSWORD');
+    $hostname = $getenv('DB_HOST');
+    $database = $getenv('DB_NAME');
+
+    $db = new PDO("pgsql:host=$hostname;dbname=$database", $username, $password);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
     return $db;

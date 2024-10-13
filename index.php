4e7f1a86e4910e2c24a8bc319fe601eb204c7c1f
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-13 20:42:09 +0300

    added favicon file

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
+<?php 
+
+require "./public/template/head.php";
+
+$title = "Index";
+
+$css = [
+    "./public/style/style.css",
+    "./public/style/index.css"
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
+<?= headerTemplate($title, $css) ?>
+
 <body>
     <h1>The Library</h1>
     <button><a href="./login.php">Login</a></button>
--- public/template/head.php
+++ public/template/head.php
@@ -5,11 +5,13 @@ function headerTemplate ($title, $cssfiles)  {
     foreach($cssfiles as $cssfile) {
         $csslink = "$csslink<link rel='stylesheet' href='$cssfile'>";
     }
+    $favicon = $_SERVER['DOCUMENT_ROOT']."/public/images/favicon.ico";
     $head =<<<END
         <head>
             <meta author="Derrick Shibero Wakhu">
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="shortcut icon" href="$favicon" type="image/x-icon">
             <title>$title</title>
             $csslink
         </head>
@@ -18,4 +20,6 @@ function headerTemplate ($title, $cssfiles)  {
 }
 
 ?>
+
+
 

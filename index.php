b07908de599633a7a14f2a07e0da8860f11a103b
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-14 09:02:02 +0300

    tried media queries

--- index.php
+++ index.php
@@ -17,8 +17,22 @@ $css = [
 <?= headerTemplate($title, $css) ?>
 
 <body>
-    <h1>The Library</h1>
-    <button><a href="./login.php">Login</a></button>
-    <button><a href="./signup.php">Sign up</a></button>
+
+    <header>
+
+        <h1>The Library</h1>
+
+        <div id="formbtn">
+            <form action="index.php" method="post">
+
+                <input type="submit" value="signup" name="signup">
+                <input type="submit" value="Login" name="login">
+
+            </form>
+        </div>
+        
+    </header>
+    <!-- <button><a href="./login.php">Login</a></button>
+    <button><a href="./signup.php">Sign up</a></button> -->
 </body>
 </html>

ea3a8368eda7ac79a7e8d110ce3b0f22748636d2
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-16 23:57:07 +0300

    added some css code

--- public/function/login/login.ts
+++ public/function/login/login.ts
@@ -3,13 +3,13 @@ const passwordInp: HTMLFormElement = document.querySelector("#password-su-inp");
 const passwordChecklist: any = document.querySelectorAll(".list-item");
 
 
-let validationRegex = [
+const validationRegex = [
     { regex: /.{8,}/ },
     { regex: /[0-9]/ },
     { regex: /[a-z]/ },
     { regex: /[A-Z]/ },
     { regex: /[^A-Za-z0-9]/ }
-]
+];
 
 passwordInp.addEventListener('keyup', () =>  {
     validationRegex.forEach((item, i)  =>  {

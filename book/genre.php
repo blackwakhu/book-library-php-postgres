0cbcb62d2139e3c9bb9166069fb68cde0acae2a4
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-25 22:42:04 +0300

    added session logins code

--- book/genre.php
+++ book/genre.php
@@ -6,6 +6,20 @@ require "../models/person.php";
 require "../models/book.php";
 require "../public/template/links.php";
 
+if ($_SESSION['person'] == null)  {
+    echo "hello";
+    header("Location: index.php");
+    exit();
+}
+
+try {
+    $person = unserialize($_SESSION['person']);
+} catch (Exception $e) {
+    // Handle unserialization errors gracefully
+    echo "Error: Unable to unserialize session data.";
+    exit();
+}
+
 $new_status = "";
 
 if (isset($_POST["add_genre"]))  {

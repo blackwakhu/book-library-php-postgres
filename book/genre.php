b9a8b906f7894446f07fd4947afeb647bb4ac392
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-25 17:57:42 +0300

    added boiler to genre.php

---
+++ book/genre.php
@@ -0,0 +1,46 @@
+<?php 
+
+session_start();
+
+require "../person/person.php";
+require "../public/template/links.php";
+
+
+if (isset($_POST["login"]))  {
+    $person = Person::get_Person($_POST["uname"], $_POST["passwrd"]);
+    if (!empty($person)) {
+        $_SESSION['person'] = serialize($person);
+        header("Location: ../dashboard.php");
+        exit();
+    }
+}
+
+if (isset($_POST["signup"]))  {
+    header("Location: signup.php");
+    exit();
+}
+
+$title = "genre";
+$css = [
+    "../public/style/style.css"
+];
+
+$js = [
+    "../public/function/function.js"
+]
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?= headerTemplate($title, $css) ?>
+
+<body>
+    
+    <?= getFunction($js) ?>
+    
+</body>
+</html>

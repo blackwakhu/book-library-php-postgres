0943522faf76ef28eec207f124cdefb7de54bec9
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-28 00:20:26 +0300

    added more functions in book_genre.php

--- models/book_genre.php
+++ models/book_genre.php
@@ -2,8 +2,21 @@
 
 require "./book.php";
 require "./genre.php";
+require "../database/database.php";
 
 class BookGenre {
+    private $book;
+    private $genre;
+
+    public function __construct (Book $book, Genre $genre)  {
+        $this->book = $book;
+        $this->genre = $genre;
+    }
+
+    public static function fromBook (int $book)  {
+        $sql = "select genre from book_genre where book = ?";
+        $data = "";
+    }
 
 }
 

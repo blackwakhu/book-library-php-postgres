c7ed47f7397dc77a450108aba2a40db104f8782b
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-28 17:23:55 +0300

    starting a new function NotGenre function

--- models/book_genre.php
+++ models/book_genre.php
@@ -5,15 +5,18 @@ require "./genre.php";
 require "../database/database.php";
 
 class BookGenre {
+    // this will save the book and the genre
     private $book;
     private $genre;
 
     public function __construct (Book $book, Genre $genre)  {
+        // this is the constructor of the BookGenre class
         $this->book = $book;
         $this->genre = $genre;
     }
 
     public function Save ()  {
+        // this will save the book and genre
         $sql = "insert into book_genre (genre, book) values (?, ?)";
         $dataItems = [
             $this->book->getISBN(),
@@ -22,6 +25,12 @@ class BookGenre {
         saveToTable($sql, $dataItems);
     }
 
+    public static function NotGenre ($isbn)  {
+        // this will return the genres that are not of the same book
+    }
+
 }
+
+
 
 ?>

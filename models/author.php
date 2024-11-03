99e5f82fa8226de53cadca461d994125b78672b3
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-04 02:01:19 +0300

    starting the select statement for all records in hte author table

--- models/author.php
+++ models/author.php
@@ -3,14 +3,26 @@
 require_once "../database/database.php";
 
 class Author  {
+    private $id;
     private $fname;
     private $lname;
     private $bio;
 
-    public function __construct (string $fname, string $lname, string $bio)  {
+    public function __construct (int $id, string $fname, string $lname, string $bio)  {
+        $this->id = $id;
         $this->fname = $fname;
         $this->lname = $lname;
         $this->bio = $bio;
+    }
+
+    public static function displayAll ()  {
+        $sql = "select * from author order by fname";
+        $data = selectAllDatabase($sql, []);
+        $authors = [];
+        
+        foreach ($data as $datum)  {
+            
+        }
     }
 }
 

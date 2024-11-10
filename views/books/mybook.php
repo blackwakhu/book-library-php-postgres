fd795c26e1bc55f44b33a1a758a2155791a2e648
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-11 00:54:12 +0300

    added the skeleton

--- views/books/mybook.php
+++ views/books/mybook.php
@@ -1,5 +1,28 @@
 <?php
 
-require_once "";
+require_once "../../models/book.php";
+require_once "../../public/template/links.php";
+
+$title = "mybook.php";
+
+$css = [
+    "../../public/style/style.css",
+    "../../public/style/books.css"
+];
+
+$js = [
+    "../../public/function/function.js",
+    "../../public/function/books.js"
+]
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?= headerTemplate($title, $css) ?>
+    <body>
+    
+        <?= getFunction($js) ?>
+
+    </body>
+</html>

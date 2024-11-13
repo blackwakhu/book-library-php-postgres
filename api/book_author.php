9dd4557c26e3c0316ca81f424420c5b4a32d7e1c
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-13 11:51:08 +0300

    problems with cors

--- api/book_author.php
+++ api/book_author.php
@@ -1,5 +1,9 @@
 <?php 
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
 require_once "../models/book.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST")  {
